8b4eda898d175445763b50fc8b9c41799b0e4bb1
Tristan Brown <tgbrown@stanford.edu>
2018-07-17 23:52:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function () {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(press) {
+    if (press.which === 71) {
+      alert("You have entered g.");
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}// call f/define functions here
 
 $(document).ready(function(){
-
-// call functions here
 
 });
