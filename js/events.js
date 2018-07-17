69c55dba29f7e11354360b8ccea3b615b5768add
Tristan Brown <tgbrown@stanford.edu>
2018-07-17 23:49:40 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,30 @@
 
 $(document).ready(function(){
 
-// call functions here
+  function getIt() {
+    $('p').on("click", function () {
+      alert("Hey!");
+    })
+  }
+  
+  function frameIt() {
+    $('img').on("load", function() {
+      $('img').addClass("tasty");
+    })
+  }
+  
+  function pressIt() {
+    $('#typing').on("keydown", function(press) {
+      if (press.which === 71) {
+        alert("You have entered g.");
+      }
+    })
+  }
+  
+  function submitIt() {
+    $('form').on("submit", function() {
+      alert("Your form is going to be submitted now.");
+    })
+  }// call functions here
 
 });
